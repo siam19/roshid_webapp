539782a11a7686464d34a951c89f8b52f6e3f365
Siam Hasan <siam.hasan.19@gmail.com>
2024-03-03 08:39:23 +0600

    added metrics and order table ui components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1 class="bg-blue-600">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Metrics from "./metrics.svelte";
+    import OrderTable from "./order-table.svelte";
+</script>
+
+
+
+<h1 class=" text-blue-600 text-center text-4xl m-4">Roshid</h1>
+
+
+<!--create a list of all the pages ("New Order" , "Create Product", "Setup Delivery) -->
+
+<ul class="flex justify-center">
+    <li class="m-4">
+        <a href="/new-order" class="text-blue-600">New Order</a>
+    </li>
+    <li class="m-4">
+        <a href="/create-product" class="text-blue-600">Create Product</a>
+    </li>
+    <li class="m-4">
+        <a href="/setup-delivery" class="text-blue-600">Setup Delivery</a>
+    </li>
+</ul>
+
+<Metrics/>
+<OrderTable/>
+```
+
---
+++ src/routes/order-table.svelte
@@ -0,0 +1,132 @@
+<link
+	href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
+	rel="stylesheet">
+	<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<div class="flex items-center justify-center text-xl text-blue-500 py-3 ">
+    <h2>Recent Orders</h2>
+</div> 
+<div class="flex items-center justify-center mx-auto px-6 max-w-6xl">
+     
+	<div class="col-span-12">
+		<div class="overflow-auto lg:overflow-visible ">
+			<table class="table text-gray-400 border-separate space-y-6 text-sm">
+				<thead class="bg-gray-100 text-gray-950">
+					<tr>
+						<th class="p-3 text-left">Name</th>
+						<th class="p-3 text-left">Category</th>
+						<th class="p-3 text-left">Price</th>
+						<th class="p-3 text-left">Status</th>
+						<th class="p-3 text-left">Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="bg-gray-100">
+						<td class="p-3">
+							<div class="flex align-items-center">
+								
+								<div class="ml-3">
+									<div class="text-gray-900">Asadur Rahman</div>
+									<div class="text-gray-400">23/A, Basundhara Residnetial, Dhaka</div>
+								</div>
+							</div>
+						</td>
+						<td class="p-3">
+							Technology
+						</td>
+						<td class="p-3 font-bold">
+							200.00$
+						</td>
+						<td class="p-3">
+							<span class="bg-green-400 text-gray-50 rounded-md px-2">available</span>
+						</td>
+						<td class="p-3">
+
+							<a href="#" class="text-gray-400 hover:text-gray-100  mx-2">
+								<i class="material-icons-outlined text-base">edit</i>
+							</a>
+
+						</td>
+					</tr>
+					<tr class="bg-gray-100">
+						<td class="p-3">
+							<div class="flex align-items-center">
+								
+								<div class="ml-3">
+									<div class="text-gray-900">Jamil Karim</div>
+									<div class="text-gray-400">56/A, West Nakhalpara, Satmojil road, Dhaka</div>
+								</div>
+							</div>
+						</td>
+						<td class="p-3">
+							Technology
+						</td>
+						<td class="p-3 font-bold">
+							200.00$
+						</td>
+						<td class="p-3">
+							<span class="bg-red-400 text-gray-50 rounded-md px-2">no stock</span>
+						</td>
+						<td class="p-3">
+							
+							<a href="#" class="text-gray-400 hover:text-gray-100 mx-2">
+								<i class="material-icons-outlined text-base">edit</i>
+							</a>
+						
+						</td>
+					</tr>
+					<tr class="bg-gray-100">
+						<td class="p-3">
+							<div class="flex align-items-center">
+								
+								<div class="ml-3">
+									<div class="text-gray-900">Asadur Rahman</div>
+									<div class="text-gray-400">23/A, Basundhara Residnetial, Dhaka</div>
+								</div>
+							</div>
+						</td>
+						<td class="p-3">
+							Technology
+						</td>
+						<td class="p-3 font-bold">
+							200.00$
+						</td>
+						<td class="p-3">
+							<span class="bg-yellow-400 text-gray-50  rounded-md px-2">start sale</span>
+						</td>
+						<td class="p-3">
+				
+							<a href="#" class="text-gray-400 hover:text-gray-100 mx-2">
+								<i class="material-icons-outlined text-base">edit</i>
+							</a>
+						
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+
+<style>
+	.table {
+		border-spacing: 0 10px;
+	}
+
+	i {
+		font-size: 1rem !important;
+	}
+
+	.table tr {
+		border-radius: 20px;
+	}
+
+	tr td:nth-child(n+5),
+	tr th:nth-child(n+5) {
+		border-radius: 0 .625rem .625rem 0;
+	}
+
+	tr td:nth-child(1),
+	tr th:nth-child(1) {
+		border-radius: .625rem 0 0 .625rem;
+	}
+</style>
